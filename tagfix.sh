8eef4b70a69bb3b5b77a77fec01087eea0ffaf4e
Isaac Rhett <izcet@users.noreply.github.com>
2025-03-14 01:39:34 -0400

    tag fix

---
+++ tagfix.sh
@@ -0,0 +1,3 @@
+find . -name "*.md" | xargs -I % sed -i 's/#Vim #Script/#VimScript/' %
+find . -name "*.md" | xargs -I % sed -i 's/#C++/#CPP/' %
+find . -name "*.md" | xargs -I % sed -i 's/#C#/#Csharp/' %
